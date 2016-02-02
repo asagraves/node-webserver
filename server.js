708aba9b20cc1b4982eaabd62d4e5ade4f3a4ef2
Andrew Sagraves <andrewsagraves@gmail.com>
2016-02-02 09:23:45 -0600

    added more server side dev

--- server.js
+++ server.js
@@ -1,16 +1,19 @@
 'use strict';
 const http = require('http');
-const { PORT } = process.env;
+const PORT = process.env.PORT || 3000;
 console.log(process.env);
 
 http.createServer((req, res) => {
   console.log(req.method, req.url);
 
-  res.writeHead(200, {
-    'Content-type': 'text/html'
-});
+  if (request.url === '/hello') {
+    res.end('<h1>what up ma?</h1>');
+} else if (req.url === '/random'); {
+    re.end(Math.random().toString());
+} else {
+  res.end('access denied!');
+}
 
-  res.end('<h1>what up ma?</h1>');
 }).listen(PORT, () => {
-  console.log('Node.js started. Listening on port ${PORT}');
+  console.log(`Node.js started. Listening on port ${PORT}`);
 });
