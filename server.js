588e38247df0a3dbf197be37ec274e656d2fc7f9
Andrew Sagraves <andrewsagraves@gmail.com>
2016-02-02 10:06:42 -0600

    added timeout functionality

--- server.js
+++ server.js
@@ -6,13 +6,32 @@ console.log(process.env);
 http.createServer((req, res) => {
   console.log(req.method, req.url);
 
-  if (request.url === '/hello') {
-    res.end('<h1>what up ma?</h1>');
-} else if (req.url === '/random'); {
-    re.end(Math.random().toString());
-} else {
-  res.end('access denied!');
-}
+  if (req.url === '/hello') {
+    const msg = `<h1>hello world</h1>
+    <h2>goobye world</h2>
+    `;
+      res.writeHead(200, {
+        'Content-Type': 'text/html'
+      });
+      let time = 1000;
+
+    msg.split('').forEach((char, i) => {
+      setTimeout(() => {
+      res.write(char);
+     }, 1000 * i);
+    });
+
+      setTimeout(() => {
+        res.end();
+      }, 20000);
+
+  } else if (req.url === '/random') {
+    res.end(Math.random().toString());
+  } else {
+    res.writeHead(403);
+    res.end('access denied!');
+  }
+
 
 }).listen(PORT, () => {
   console.log(`Node.js started. Listening on port ${PORT}`);
