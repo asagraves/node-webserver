4e6b720aaec676f9a5a0c816115013dea9f0b49b
Andrew Sagraves <andrewsagraves@gmail.com>
2016-02-02 11:11:07 -0600

    initial express added

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 'use strict';
-const http = require('http');
+const express = require('express');
+const app = express();
+
 const PORT = process.env.PORT || 3000;
 console.log(process.env);
 
@@ -7,32 +9,52 @@ http.createServer((req, res) => {
   console.log(req.method, req.url);
 
   if (req.url === '/hello') {
-    const msg = `<h1>hello world</h1>
+
+  } else if (req.url === '/random') {
+  } else {
+
+  }
+})
+
+app.get('/hello', (req, res) => {
+  const msg = `<h1>hello world!</h1>
     <h2>goobye world</h2>
     `;
       res.writeHead(200, {
         'Content-Type': 'text/html'
       });
       let time = 1000;
-
     msg.split('').forEach((char, i) => {
       setTimeout(() => {
       res.write(char);
      }, 1000 * i);
     });
-
       setTimeout(() => {
         res.end();
       }, 20000);
-
-  } else if (req.url === '/random') {
-    res.end(Math.random().toString());
-  } else {
-    res.writeHead(403);
-    res.end('access denied!');
-  }
+})
 
 
-}).listen(PORT, () => {
+
+app.get('/random', (req, res) => {
+  res.end(Math.random().toString());
+});
+
+//all means all verbs and '*' means everything
+app.all('*', (req, res) => {
+  res.writeHead(403);
+  res.end('access denied!');
+})
+
+
+app.listen(PORT, () => {
   console.log(`Node.js started. Listening on port ${PORT}`);
 });
+
+
+
+
+
+
+
+
