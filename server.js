2e81ef1e11ab74c0019775dc3fb0ba01fca71751
Andrew Sagraves <andrewsagraves@gmail.com>
2016-02-03 10:45:39 -0600

    update with jade

--- server.js
+++ server.js
@@ -1,25 +1,34 @@
 'use strict';
-const express = require('express');
+
 const app = express();
-
 const PORT = process.env.PORT || 3000;
-console.log(process.env);
 
+app.set('view engine', 'jade');
+
+
+app.get('/', (req, res) => {
+  res.render('index', {
+    title: 'Super cool app',
+    date: new Date()
+  });
+});
 
 
 app.get('/hello', (req, res) => {
-  const name = req.query.name;
-  const msg = `<h1>hello {name} !</h1>
-    <h2>goobye {name}</h2>`;
-    // console.log('QUERY PARAMS>>>>', req.query)
+  const name = req.query.name || 'World';
+  const msg = `<h1>Hello ${name}!</h1>
+<h2>goodbye ${name}!</h2>`;
+
       res.writeHead(200, {
         'Content-Type': 'text/html'
       });
+
     msg.split('').forEach((char, i) => {
       setTimeout(() => {
       res.write(char);
      }, 1000 * i);
     });
+
       setTimeout(() => {
         res.end();
       }, msg.length * 1000 + 2000);
